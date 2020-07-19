b1e21eb24d461db08b1124468f076477a10a712d
faiq.muhammad <faiq.muhammad@careem.com>
2020-07-19 21:11:10 +0500

    Added test file

--- src/main/java/GameOfLife.kt
+++ src/main/java/GameOfLife.kt
@@ -1,2 +1,10 @@
 class GameOfLife {
+
+    companion object{
+
+        fun applyRule(grid: Array<IntArray>): Array<IntArray>{
+
+            return grid
+        }
+    }
 }
--- src/test/java/GameOfLifeTest.kt
+++ src/test/java/GameOfLifeTest.kt
@@ -1,16 +1,21 @@
-import org.junit.Assert.*
+import GameOfLife.Companion.applyRule
 import org.junit.Test
 import org.junit.internal.runners.JUnit4ClassRunner
 import org.junit.runner.RunWith
-import org.junit.runner.Runner
+
 
 @RunWith(JUnit4ClassRunner::class)
 class GameOfLifeTest{
 
-    val grid = Array(10){ IntArray(10) {0}}
+    var grid = arrayOf(intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 1, 1, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 1, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+
 
     @Test
     fun `Any live cell with fewer than two live neighbors dies, as if caused by under population`(){
 
+        val gameOfLife = GameOfLife.applyRule(grid)
+
     }
+
+
 }
