8dbcb31d979aa9c36990c781e99715101a004231
faiq.muhammad <faiq.muhammad@careem.com>
2020-07-20 23:04:33 +0500

    Added get neighbours

--- src/main/java/GameOfLife.kt
+++ src/main/java/GameOfLife.kt
@@ -1,10 +1,66 @@
 class GameOfLife {
 
-    companion object{
 
-        fun applyRule(grid: Array<IntArray>): Array<IntArray>{
+    companion object {
+
+        fun applyRule(grid: Array<IntArray>): Array<IntArray> {
+
+            grid.forEachIndexed { rowIndex, rowItem ->
+                rowItem.forEachIndexed { colIndex, colItem ->
+                    {
+                        applyUnderPopulationRule(grid, rowIndex, colIndex)
+                    }
+                }
+            }
 
             return grid
         }
+
+        private fun applyUnderPopulationRule(grid: Array<IntArray>, rowIndex: Int, colIndex: Int) {
+
+        }
+
+    }
+
+
+    fun getNeighbours(grid: Array<IntArray>, row: Int, col: Int): List<Int> {
+
+        val list: MutableList<Int> = mutableListOf()
+
+        if (col + 1 < grid[0].size) {
+            list.add(grid[row][col + 1])
+        }
+
+        if (col + 1 < grid[0].size && row + 1 < grid.size) {
+            list.add(grid[row + 1][col + 1])
+        }
+
+        if (row + 1 < grid.size) {
+            list.add(grid[row + 1][col])
+        }
+
+        if (col - 1 >= 0 && row + 1 < grid.size) {
+            list.add(grid[row + 1][col - 1])
+        }
+
+        if (col - 1 >= 0) {
+            list.add(grid[row][col - 1])
+        }
+
+        if (col - 1 >= 0 && row - 1 >= 0) {
+            list.add(grid[row - 1][col - 1])
+        }
+
+        if (row - 1 >= 0) {
+            list.add(grid[row - 1][col])
+        }
+
+        if (col + 1 < grid[0].size && row - 1 >= 0) {
+            list.add(grid[row - 1][col + 1])
+        }
+
+        return list
+
+
     }
 }
--- src/test/java/GameOfLifeTest.kt
+++ src/test/java/GameOfLifeTest.kt
@@ -2,19 +2,64 @@ import GameOfLife.Companion.applyRule
 import org.junit.Test
 import org.junit.internal.runners.JUnit4ClassRunner
 import org.junit.runner.RunWith
+import kotlin.test.assertEquals
 
 
 @RunWith(JUnit4ClassRunner::class)
-class GameOfLifeTest{
+class GameOfLifeTest {
 
-    var grid = arrayOf(intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 1, 1, 0, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 1, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0, 0), intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+    var grid = arrayOf(
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 1, 1, 0, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 1, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+            intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
 
 
     @Test
-    fun `Any live cell with fewer than two live neighbors dies, as if caused by under population`(){
+    fun `Any live cell with fewer than two live neighbors dies, as if caused by under population`() {
 
-        val gameOfLife = GameOfLife.applyRule(grid)
+        val gameOfLife = applyRule(grid)
 
+        var expected = arrayOf(
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 0, 1, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 1, 1, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 1, 1, 0, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0),
+                intArrayOf(0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+
+        equals(expected, gameOfLife)
+
+    }
+
+    @Test
+    fun `test get neighbours`() {
+        val gameOfLife = GameOfLife()
+
+        val actual = gameOfLife.getNeighbours(grid, 9, 0)
+
+
+        assert(actual.isNotEmpty())
+
+        assertEquals(listOf(0, 0, 0), actual)
+
+
+    }
+
+    private fun equals(expected: Array<IntArray>, actual: Array<IntArray>) {
+        expected.forEachIndexed { rowIndex, rowItem ->
+            assert(rowItem contentEquals actual[rowIndex])
+        }
     }
 
 
